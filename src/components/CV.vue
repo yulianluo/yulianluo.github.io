1620c8ef0b75a5aad07619eee6f4af4250378070
yulianluo <yulianlluo@gmail.com>
2022-08-30 20:05:05 +0200

    test

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    hi
+    hi hi
   <!-- <iframe height="100%" width=100% src="../assets/ixp.png" ></iframe> -->
   <embed src="https://drive.google.com/file/d/1tV0MXDJW7DjtkiS9elJsS_CGFUXZkhrc/view?usp=sharing" width="100%" height="700px"  /> 
 <embed src="https://drive.google.com/file/d/1tV0MXDJW7DjtkiS9elJsS_CGFUXZkhrc/view?usp=sharing" width="100%" height="700px"  /> 
